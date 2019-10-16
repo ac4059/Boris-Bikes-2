d7557328df32753feec7c2931fe20c6817d8d65d
liatmoss <liatmoss@hotmail.com>
2019-10-16 15:58:59 +0100

    Completed 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,11 +3,13 @@ require './lib/bike.rb'
 
 class DockingStation
   attr_reader :bike
-  def release_bike
-    Bike.new
-  end
 
   def dock(bike)
     @bike = bike
+  end
+
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,11 +5,11 @@ describe DockingStation do
   it "releases a bike" do
     DockingStation.respond_to?(:release_bike)
   end
-  it "releases a working bike" do
-    docking_station = DockingStation.new
-    bike = docking_station.release_bike
-    expect(bike).to be_working
-  end
+  #it "releases a working bike" do
+    #docking_station = DockingStation.new
+    #bike = docking_station.release_bike
+    #expect(bike).to be_working
+  #end
 end
 describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
@@ -21,4 +21,20 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+
+    end
+
+  end
+
 end
